96b5d1debc4423c5ab2aea081002cbd5926eaf70
Daniel Alejandro Otero <alejandroogbmediagroup@gmail.com>
2021-03-08 16:09:31 -0500

    Acercamiento a vuejs

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header-component/>
+    <br>
+    <img alt="Vue logo" src="./assets/nieva.jpg" />
+    <HelloWorld msg="D11" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HeaderComponent from "./components/headerComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HeaderComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+img {
+  width: 25%;
+  height: 25%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>{{ description }}</h3>
+    <p>
+      Daniel Otero is a young player of soccer of Sel. Valle and Colombia
+      Paralimpic.
+      <a
+        href="https://www.youtube.com/watch?v=g1T1JBZSkB0"
+        target="_blank"
+        rel="noopener"
+        >Skills Daniel Otero</a
+      >.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      description:
+        "His position is midfielder, he have very big talents is vision, assistants and goals.",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
